a9fa21ee3bdb8d7d0b65f3eb65c37b8b144a4991
jeanetteobr <jeanette.obr@gmail.com>
2021-08-17 15:26:59 -0400

    Add Book, Author, Category models.

--- books/models.py
+++ books/models.py
@@ -8,3 +8,34 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.ManyToManyField('Author', related_name='authors')
+    description= models.CharField(max_length=255)
+    book_url =  models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=False)
+    # add ManyToManyField for categories
+
+    def __str__(self):
+        return self.title
+
+class Author(models.Model):
+    name = models.CharField(max_length=225)
+    books = models.ManyToManyField(Book, related_name='book')
+
+    class Meta:
+        ordering = ['name']
+        
+    
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=75, null=True)
+
+    def __str__(self):
+        return self.category_name
